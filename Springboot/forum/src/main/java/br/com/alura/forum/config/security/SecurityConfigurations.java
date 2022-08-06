71bcdcccdb7c3f25ed51fd36e7c5f6fb23532c49
Gustavo Santos <gugahh.br@gmail.com>
2022-08-06 17:24:43 -0300

    Evoluindo Autenticação (2)

--- Springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ Springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,9 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AutenticacaoService autenticacaoService;
     // Configuracoes de Autenticacao (login, etc)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService);
     }
 
     // Configuracoes de Autorizacao (URLs, etc)
